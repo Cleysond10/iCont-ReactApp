279360ba51760f620e27b6fd12c815f31f427e5d
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-02-28 03:41:44 -0300

    Update style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 export default function index() {
   return (
     <Navbar className="navbar" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">iCount</Navbar.Brand>
+      <Navbar.Brand className="navTitle" as={Link} to="/">iCount</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Nav className="navLink1">
           <Nav.Link as={Link} to="/counter">Counter</Nav.Link>
-          <Nav.Link className="navLink" as={Link} to="/bonus">Bonus</Nav.Link>
+          <Nav.Link className="navLink2" as={Link} to="/bonus">Bonus</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
