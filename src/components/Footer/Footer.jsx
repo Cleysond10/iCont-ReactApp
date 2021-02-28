280fc32a6eb00f6d9eeb0b2db5a06c762befe117
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-02-28 03:43:19 -0300

    Update footer style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,13 +7,13 @@ export default function Footer({ children }) {
     <footer className="footer">
       <Row>
         <Col>
-          <div className="footer__social">
+          <div className="footer__In">
             <a href="https://www.linkedin.com/in/cleyson-henrique-s-88790012a/">LinkedIn</a>
           </div>
         </Col>
         <Col>
-          <div className="footer__social">
-            <a href="https://github.com/Cleysond10">GitHub Page</a>
+          <div className="footer__git">
+            <a href="https://github.com/Cleysond10/iCount-ReactApp">GitHub Page</a>
           </div>
         </Col>
         <Col>
