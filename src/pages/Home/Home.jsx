2caa5f28a6d089208f36ab5f92a045bb1b598308
Cleyson Henrique Silva Duarte <cleysonduarte99@gmail.com>
2021-03-04 01:58:31 -0300

    Add new images to carousel and edit text content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,51 +1,43 @@
 import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { Carousel } from 'react-bootstrap';
+import Img1 from './img/logo-react.png';
+import Img2 from './img/office.jpeg';
 import './Home.css';
 
 export default function Home() {
   useEffect(() => {
-    toast.success('Welcome to iCount !');
+    toast.success('Welcome !');
   }, []);
 
   return (
     <div>
-      <h1 className="title">Welcome!</h1>
+      <h1 className="title">Welcome to iCount!</h1>
       <p className="text">
         {`
-        This is the iCount App. Here you'll find a number counter.`}
+        This is the iCount App: a smart and useful counter developed in React. Here you'll find a number counter and much more features that will match your expectations of a beginner's developer - i hope. `}
+        &#x1F648;
       </p>
       <Carousel className="carousel">
         <Carousel.Item interval={5000}>
           <img
             className="d-block w-100"
-            src="https://reactjs.org/logo-og.png"
-            alt="First slide"
+            src={Img1}
+            alt="react js"
           />
-          <Carousel.Caption>
-            <p>First slide label</p>
+          <Carousel.Caption className="cap">
+            <p>iCount is a React.js application.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={5000}>
           <img
             className="d-block w-100"
-            src="https://lh3.googleusercontent.com/proxy/OzJfmff2Hbm8KZ8r86PLJRG_hbXovdCy4OU0X-wb5Lk393Iz2fbZdjBhW3iIgoWf1FJFMa-1LqVpGDBe66WXqrXWW7Ix_8mYN0lWX_obLlEWZBKXF_pl-ervAMN2nbHP81C67XF0xNSk6A"
-            alt="Second slide"
+            src={Img2}
+            alt="office"
           />
 
-          <Carousel.Caption>
-            <p>Second slide label</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <img
-            className="d-block w-100"
-            src="https://cdn.stocksnap.io/img-thumbs/960w/coding-programming_U3W2SHOLWQ.jpg"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <p>Third slide label</p>
+          <Carousel.Caption className="cap">
+            <p>Visit my social media below.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
